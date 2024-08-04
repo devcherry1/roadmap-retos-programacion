6020d6d074dedb716508e01545358f3ff2db286a
HodginsOhannigan <62486573+devcherry1@users.noreply.github.com>
2024-08-04 13:39:16 -0400

    devcherry1.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/devcherry1.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+# hashtag o numeral: comentario de linea completa 
+"""
+esto es un comentario 
+en varias lineas
+"""
+'''
+tambien se pueden usar comillas simples
+para un comentario multilinea
+en tanto sean 3 para abrir y 3 para cerrar
+'''
+#variable
+mi_variable = "ejemplo"
+#Python no tiene constantes pero se nombra la variable en mayusculas 
+#para sugerir que no debe ser cambiado
+MI_CONSTANTE = "no ignores las mayusculas"
+#***Tipos de datos primitivos***
+#Numeros enteros o decimales, no llevan comillas
+my_int = 2
+my_float = 1.5
+#Valores booleanos, no llevan comillas (true o false)
+my_bool = True
+#las comillas se usan para cadenas de texto o string
+my_str = "Mambru se fue a la guerra"
+#imprimir en consola, le pasamos una variable pero se puede usar str int o float
+print(my_str)
